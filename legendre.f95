4d1c76cc5b2a5129f92002834e6d0aaf012463de
Nguyen Le Anh <75296157+nlanh@users.noreply.github.com>
2022-01-28 15:20:55 +0700

    Calculation of Legendre polynomials

--- legendre.f95
+++ legendre.f95
@@ -4,15 +4,13 @@ implicit none
 
 real*16 x,pleg,xmin,xmax,step
 
-integer*16 j,n,k,nmax,fac1,fac2,fac3,fac4
+integer*16 j,n,k,n2,fac1,fac2,fac3,fac4
 integer ix,ni
 
 open(unit=1, file='pleg.dat')
 
 print*, 'Enter n (order of legendre polynomial, hint: 2)'
 read*, n
-print*, 'Enter nmax (infinity index for series, hint: 20)'
-read*, nmax
 print*, 'Enter xmin (maximum range of x for output, hint: -1)'
 read*, xmin
 print*, 'Enter xmax (maximum range of x for output, hint: 1)'
@@ -20,12 +18,15 @@ read*, xmax
 print*, 'Enter step (step of x, hint: 0.01)'
 read*, step
 
+if(mod(n,2).eq.0)n2=n/2
+if(mod(n,2).eq.1)n2=(n-1)/2
+
 ni = int((xmax-xmin+0.00000001)/step)
-nmax = nmax/2
+
 do ix=0,ni
 	pleg=0
 	x=xmin+ix*step
-	do k=0,nmax
+	do k=0,n2
 	    fac1=1
 		do j=1,k
 			fac1=fac1*j
