e41c58bdfd62453ed0d3afaf4b29eb066043c31f
Nguyen Le Anh <75296157+nlanh@users.noreply.github.com>
2022-03-31 04:36:01 +0700

    Spherical bessel and neumann functions
    Updated

--- jyn.for
+++ jyn.for
@@ -5,15 +5,14 @@
       integer i,j,n,ni
       dimension x(10000),ayn(10000),dyn(10000),ajn(10000),djn(10000)
       
-      open(unit=1, file='y.dat') 
-      open(unit=2, file='yjn.inp')
+      open(unit=1, file='jyn.dat')
 
-!print*, 'Nhap n'
-      read(2,*) n
-!print*, 'Nhap xmax'
-      read(2,*) xmax
-!print*, 'Nhap step'
-      read(2,*) step
+      print*, 'Enter n'
+      read*, n
+      print*, 'Enter xmax'
+      read*, xmax
+      print*, 'Enter step'
+      read*, step
 
       ni = int((xmax+0.00000001)/step)
       
@@ -40,32 +39,28 @@
 
       endprogram
       
-      SUBROUTINE DER1(Y,X,N,H)                                            
-C NUMERICAL DERIVATION OF THE FUNCTION Y KNOWN AT N POINTS.             
-C THIS SUBROUTINE REQUIRES AT LEAST 7 POINTS.                           
-C AND RETURNS THE VALUE OF H*(D(Y)/DR) IN X.                            
-C***********************************************************************
-      IMPLICIT REAL*8 (A-H,O-Z)
-      DIMENSION X(N),Y(N)
-      N3=N-3
-      X(1)=(-147.D0*Y(1)+360.D0*Y(2)-450.D0*Y(3)+400.D0*Y(4)-225.D0*Y(5)
-     1+72.D0*Y(6)-10.D0*Y(7))/60.D0                                     
-      X(2)=(-10.D0*Y(1)-77.D0*Y(2)+150.D0*Y(3)-100.D0*Y(4)+50.D0*Y(5)-15
-     1.D0*Y(6)+2.D0*Y(7))/60.D0                                         
-      X(3)=(2.D0*Y(1)-24.D0*Y(2)-35.D0*Y(3)+80.D0*Y(4)-30.D0*Y(5)+8.D0*Y
-     1(6)-Y(7))/60.D0                                                   
-      DO I=4,N3                                                       
-      X(I)=(45.D0*(Y(I+1)-Y(I-1))-9.D0*(Y(I+2)-Y(I-2))+Y(I+3)-Y(I-3))/60
-     1.D0
-      END DO                                                               
-      X(N-2)=(Y(N-6)-8.D0*Y(N-5)+30.D0*Y(N-4)-80.D0*Y(N3)+35.D0*Y(N-2)+2
-     14.D0*Y(N-1)-2.D0*Y(N))/60.D0                                      
-      X(N-1)=(-2.D0*Y(N-6)+15.D0*Y(N-5)-50.D0*Y(N-4)+100.D0*Y(N3)-150.D0
-     1*Y(N-2)+77.D0*Y(N-1)+10.D0*Y(N))/60.D0                            
-      X(N)=(10.D0*Y(N-6)-72.D0*Y(N-5)+225.D0*Y(N-4)-400.D0*Y(N3)+450.D0*
-     1Y(N-2)-360.D0*Y(N-1)+147.D0*Y(N))/60.D0
-      DO I = 1, N
-       X(I) = X(I)/H
-      END DO
-      RETURN
-      END
+      subroutine der1(y,x,n,h)                                            
+      implicit real*8 (a-h,o-z)
+      dimension x(n),y(n)
+      n3=n-3
+      x(1)=(-147.d0*y(1)+360.d0*y(2)-450.d0*y(3)+400.d0*y(4)-225.d0*y(5)
+     1+72.d0*y(6)-10.d0*y(7))/60.d0                                     
+      x(2)=(-10.d0*y(1)-77.d0*y(2)+150.d0*y(3)-100.d0*y(4)+50.d0*y(5)-15
+     1.d0*y(6)+2.d0*y(7))/60.d0                                         
+      x(3)=(2.d0*y(1)-24.d0*y(2)-35.d0*y(3)+80.d0*y(4)-30.d0*y(5)+8.d0*y
+     1(6)-y(7))/60.d0                                                   
+      do i=4,n3                                                       
+      x(i)=(45.d0*(y(i+1)-y(i-1))-9.d0*(y(i+2)-y(i-2))+y(i+3)-y(i-3))/60
+     1.d0
+      end do                                                               
+      x(n-2)=(y(n-6)-8.d0*y(n-5)+30.d0*y(n-4)-80.d0*y(n3)+35.d0*y(n-2)+2
+     14.d0*y(n-1)-2.d0*y(n))/60.d0                                      
+      x(n-1)=(-2.d0*y(n-6)+15.d0*y(n-5)-50.d0*y(n-4)+100.d0*y(n3)-150.d0
+     1*y(n-2)+77.d0*y(n-1)+10.d0*y(n))/60.d0                            
+      x(n)=(10.d0*y(n-6)-72.d0*y(n-5)+225.d0*y(n-4)-400.d0*y(n3)+450.d0*
+     1y(n-2)-360.d0*y(n-1)+147.d0*y(n))/60.d0
+      do i = 1, n
+       x(i) = x(i)/h
+      end do
+      return
+      end
